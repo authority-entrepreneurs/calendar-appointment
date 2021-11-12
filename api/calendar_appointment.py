241dc72bc440ff200ae1890a0ed03a7bd638c0f6
Animesh <bharatshrestha925@gmail.com>
2021-11-12 21:24:05 +0545

    made some changes acc to new requirements

--- api/calendar_appointment.py
+++ api/calendar_appointment.py
@@ -1,6 +1,7 @@
 import json
 
 import requests
+from requests.api import put
 
 
 class GoHighLevel:
@@ -11,6 +12,36 @@ class GoHighLevel:
             'Content-Type': 'application/json'
         }
     
+    def get_contact_lookup(self, phone):
+
+        contact_lookup = 'https://rest.gohighlevel.com/v1/contacts/lookup?phone={0}'.format(phone)
+        response = requests.request("GET", contact_lookup, 
+                                    headers=self.cred_headers)
+        result = response.json().get('contacts')[0]
+        contacts_id = result.get('id')
+        tags = result.get('tags')
+        return contacts_id, tags
+     
+    def put_contact_tags(self, contact_id, tags, apptDate):
+
+        put_contact_endpoint = 'https://rest.gohighlevel.com/v1/contacts/{0}'.format(contact_id)
+        if "outside_of_time_slot" not in tags:
+            tags.append("outside_of_time_slot")
+        payload = {
+            "tags":tags,
+            "customField":[
+                {
+                "id": "HPx3074jyCnmTheXupv6",
+                "value": apptDate
+                }
+            ]
+        }
+        response = requests.request("PUT", put_contact_endpoint, 
+                                    headers=self.cred_headers, 
+                                    data=json.dumps(payload))
+        return response.json()
+
+
     def post_appointment(self, phone, apptDate):
 
         appointment_url = "https://rest.gohighlevel.com/v1/appointments/"
@@ -24,6 +55,12 @@ class GoHighLevel:
         response = requests.request("POST", appointment_url, 
                                     headers=self.cred_headers, 
                                     data=json.dumps(payload))
-        return response.json()
+        if response.json().get('selectedSlot') is None:
+            return response.json()
+        else:
+            new_phone_format = '+1'+''.join(e for e in phone if e.isalnum())
+            contact_ids, tags = self.get_contact_lookup(new_phone_format)
+            put_response = self.put_contact_tags(contact_ids, tags, apptDate)
+            return put_response
 
 go_high = GoHighLevel()
